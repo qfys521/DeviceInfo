78e4d1a57ff3f57f4dce6b0ca4721a1c688e2ed7
qfys521 <qfys520@vip.qq.com>
2025-02-10 20:27:12 +0800

    Initial commit

---
+++ app/src/main/java/cn/qfys521/deviceinfos/Main.kt
@@ -0,0 +1,198 @@
+package cn.qfys521.deviceinfos
+
+import android.R
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ActivityManager
+import android.app.AlertDialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.os.StatFs
+import android.telephony.SubscriptionInfo
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.Locale
+
+class Main : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val scrollView = ScrollView(this)
+        val linearLayout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+        }
+        val textView = TextView(this)
+        val button = Button(this).apply {
+            text = "操作"
+        }
+        val sb = StringBuilder()
+
+        val context = applicationContext
+        val statFs = StatFs(Environment.getExternalStorageDirectory().path)
+        val totalSize = statFs.totalBytes
+        val availableSize = statFs.availableBytes
+
+
+
+        sb.appendDeviceInfo()
+        sb.appendMemoryInfo()
+        sb.appendStorageInfo(totalSize, availableSize)
+        sb.appendFileInfo(context)
+        sb.appendCpuInfo()
+        sb.appendLocalCode()
+        textView.text = sb.toString()
+        textView.movementMethod = ScrollingMovementMethod()
+        linearLayout.addView(textView)
+        linearLayout.addView(button)
+        scrollView.addView(linearLayout)
+        setContentView(scrollView)
+
+        textView.setOnLongClickListener {
+            showLongClickDialog(textView, sb)
+            true
+        }
+
+        button.setOnClickListener {
+            showLongClickDialog(textView, sb)
+        }
+    }
+
+    private fun StringBuilder.appendDeviceInfo() {
+        append("设备名称: ").append(deviceName).append("\n")
+            .append("设备型号: ").append(modelName).append("\n")
+            .append("系统版本: ").append(systemVersion).append("\n")
+            .append("厂商: ").append(brand).append("\n")
+            .append("制造商: ").append(manufacturer).append("\n")
+            .append("SDK版本: ").append(sDKVersion).append("\n")
+            .append("系统语言: ").append(systemLanguage).append("\n")
+            .append("支持的ABI列表: ").append(Build.SUPPORTED_ABIS.contentToString()).append("\n")
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun StringBuilder.appendMemoryInfo() {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val memoryInfo = ActivityManager.MemoryInfo()
+        activityManager.getMemoryInfo(memoryInfo)
+        val mmr = memoryInfo.totalMem / (1024 * 1024)
+        append("设备内存: ").append(mmr / 1024).append(".").append(mmr % 1024).append("GB").append("\n")
+    }
+
+    private fun StringBuilder.appendStorageInfo(totalSize: Long, availableSize: Long) {
+        append("总存储: ").append(getUnit(totalSize.toDouble())).append("\n")
+        append("已用大小: ").append(getUnit(availableSize.toDouble())).append("\n")
+        append("可用大小: ").append(getUnit((totalSize - availableSize).toDouble())).append("\n")
+    }
+
+    private fun StringBuilder.appendFileInfo(context: Context) {
+        append("当前路径下的文件列表: ").append(context.fileList().contentToString()).append("\n")
+        append("当前文件路径: ").append(context.filesDir).append("\n")
+    }
+
+    private fun StringBuilder.appendCpuInfo() {
+        try {
+            val process = Runtime.getRuntime().exec("getprop ro.product.cpu.abilist64")
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            val output = reader.readLine()
+            append("设备支持的架构(getprop): ").append(output).append("\n")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun StringBuilder.appendLocalCode(){
+        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as android.telephony.TelephonyManager
+
+        val localDefaultCountryCode = Locale.getDefault().country
+        val telephonyCountryCode = telephonyManager.networkCountryIso.uppercase()
+        val contextCountryCode = resources.configuration.locales[0].country
+        val phoneSimOperatorName = telephonyManager.simOperatorName
+        val phoneSimOperator = telephonyManager.simOperator
+
+
+        append("localDefaultCountryCode: ").append(localDefaultCountryCode).append("\n")
+        append("telephonyCountryCode: ").append(telephonyCountryCode).append("\n")
+        append("contextCountryCode: ").append(contextCountryCode).append("\n")
+        append("phoneSimOperatorName: ").append(phoneSimOperatorName).append("\n")
+        append("phoneSimOperator: ").append(phoneSimOperator).append("\n")
+
+    }
+
+    private fun showLongClickDialog(textView: TextView, sb: StringBuilder) {
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("轻轻选择您的操作")
+            .setMessage("展开或者取消")
+            .setIcon(R.mipmap.sym_def_app_icon)
+            .setPositiveButton("复制") { _, _ ->
+                copyToClipboard(textView.text.toString())
+            }
+            .setNegativeButton("取消", null)
+            .setNeutralButton("展开") { _, _ ->
+                showLanguageListDialog(textView, sb)
+            }
+            .create()
+        alertDialog.show()
+    }
+
+    private fun showLanguageListDialog(textView: TextView, sb: StringBuilder) {
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("轻轻选择您的操作")
+            .setMessage(systemLanguageList.contentToString())
+            .setIcon(R.mipmap.sym_def_app_icon)
+            .setPositiveButton("添加") { _, _ ->
+                sb.append("支持的语言列表: ").append(systemLanguageList.contentToString())
+                textView.text = sb.toString()
+              //  copyToClipboard(textView.text.toString())
+            }
+            .setNegativeButton("收起") { _, _ ->
+                val str = sb.toString().replaceFirst("支持的语言列表: ", "").replaceFirst(systemLanguageList.contentToString(), "")
+                sb.clear().append(str)
+                textView.text = sb.toString()
+            }
+            .create()
+        alertDialog.show()
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("text", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, "已复制到剪切板。", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getUnit(size: Double): String {
+        var size1 = size
+        var index = 0
+        while (size1 > 1024 && index < units.size - 1) {
+            size1 /= 1024
+            index++
+        }
+        return String.format(Locale.getDefault(), " %.2f %s", size1, units[index])
+    }
+
+    private val units = arrayOf("B", "KB", "MB", "GB", "TB")
+
+    companion object {
+        val deviceName: String get() = Build.DEVICE
+        val modelName: String get() = Build.MODEL
+        val systemVersion: String get() = Build.VERSION.RELEASE
+        val brand: String get() = Build.BRAND
+        val manufacturer: String get() = Build.MANUFACTURER
+        val sDKVersion: String get() = Build.VERSION.SDK_INT.toString()
+        val systemLanguage: String get() = Locale.getDefault().language
+        val systemLanguageList: Array<Locale> get() = Locale.getAvailableLocales()
+    }
+}
